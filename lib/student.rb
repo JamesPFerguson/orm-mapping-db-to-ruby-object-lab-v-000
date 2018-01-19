c49b164d7ee6be14a640038e36ae5ce3b7abf691
James Ferguson <ncjamesferguson@gmail.com>
2018-01-19 19:23:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,4 +81,15 @@ class Student
     DB[:conn].execute(sql).map{|row| Student.new_from_db(row)}
   end
 
+  def self.first_x_students_in_grade_10(x)
+
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, x).map{|row| Student.new_from_db(row)}
+
 end
