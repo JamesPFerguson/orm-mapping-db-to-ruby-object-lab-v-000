538709c696aa8cb18fcc48a2dbc1f0e034c245ac
James Ferguson <ncjamesferguson@gmail.com>
2018-01-19 19:21:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,7 @@ class Student
     WHERE grade < 12
     SQL
 
-    DB[:conn].execute(sql).map{|row| Student.new_from_db{row)}
+    DB[:conn].execute(sql).map{|row| Student.new_from_db(row)}
   end
 
 end
